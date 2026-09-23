3155c06716294c79d25755e6e98aff5e946a226b
medbarhdad <79921785+medbarhdad@users.noreply.github.com>
2026-09-23 12:11:19 +0100

    Update participation rate in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 /* PARAMÈTRES PRINCIPAUX — modifiez ces valeurs si nécessaire. */
 window.SITE_CONFIG = {
-  participationRate: 3.00,
+  participationRate: 5.00,
   inscrits: 15801162,
   votersMode: "auto",
   electionDate: "2026-09-23",
